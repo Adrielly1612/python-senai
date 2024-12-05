ddddc34606e67fa43bfc7c1b9e0b9ec2536e2325
Adrielly1612 <adriellyrochan2@gmail.com>
2024-12-05 09:55:49 -0300

    aula 02
    .

---
+++ aula02/aula02.py
@@ -0,0 +1,14 @@
+
+print ('mmensagem')
+nome_usuario = 'Elly' # tipo -> str = string
+idade_usuario = 16 #  -> int = integer
+altura_usuario = 1.57  # tipo -> float = ponto flutuante 
+maior_idade = False # tipo -> bool = boolean
+
+print('seja bem-vindo', nome_usuario)
+Seja bem-vindo nome_usuario, sua idade é idade_usuario anos,você tem altura_usuário metros. Você é maior de idade? maior_idade
+
+print ('sua idade é', idade_usuario,'anos')
+print ('suaa idade é', idade_usuario, 'anos')
+print ('você tem', altura_usuario, metros.')
+print ('você é maior de idade?', maior_idade)       
